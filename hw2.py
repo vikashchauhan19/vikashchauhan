c67e1b640f827d289349d2ceb7278194833092f8
vikashchauhan19 <113350060+vikashchauhan19@users.noreply.github.com>
2022-10-12 23:56:19 +0530

    vv

---
+++ hw2.py
@@ -0,0 +1,16 @@
+
+# First 10 natural number
+x = 0
+while x!=10:
+    x +=1
+    print(x)
+
+# sum of all number from 1 to given numbeer
+
+n = int(input("Enter number"))
+sum = 0
+for num in range(1, n + 1, 1):
+     sum = sum + num
+print("Sum of numbers is: ", sum)
+
+
