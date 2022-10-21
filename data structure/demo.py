f08638f2945cfce4e92f4dd073eb4768ec9d53ad
vikashchauhan19 <113350060+vikashchauhan19@users.noreply.github.com>
2022-10-21 16:56:53 +0530

    image.py

--- data structure/demo.py
+++ data structure/demo.py
@@ -1,5 +1,13 @@
 #wasp to find hypotenuse using pythagoras theaorem.
-per= int(input('Enter the value of perpendicular line :'))
-bas= int(input('Entyer tyhe value of base line: '))
-hyp = (per**2 + bas**2)**0.5
-print(hyp)
+
+#function defination
+def pythagoras():
+    per = int(input('Enter the value of perpendicular line :'))
+    bas = int(input('Entyer tyhe value of base line: '))
+    hyp = (per**2 + bas**2)**0.5
+    print(hyp) 
+
+
+#function call
+pythagoras()    
+pythagoras()    
